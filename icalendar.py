789b3fcc75854206a7ea117cc5a57b248f5710f8
hoangquochung1110 <arsenalforeversince2007@gmail.com>
2023-06-05 09:12:25 +0700

    Remove icalendar. Install ics

--- icalendar.py
+++ icalendar.py
@@ -1,19 +1,14 @@
-from icalendar import Calendar, Event
-import pytz
 from datetime import datetime
-cal = Calendar()
-cal.add('prodid', '-//My calendar product//mxm.dk//')
-cal.add('version', '2.0')
+from ics import Calendar, Event
 
-event = Event()
-event.add('summary', 'Python meeting about calendaring')
-event.add('dtstart', datetime(2005,4,4,8,0,0,tzinfo=pytz.utc))
-event.add('dtend', datetime(2005,4,4,10,0,0,tzinfo=pytz.utc))
-event.add('dtstamp', datetime(2005,4,4,0,10,0,tzinfo=pytz.utc))
-cal.add_component(event)
-import tempfile, os
-directory = tempfile.mkdtemp()
-print(f"{directory=}")
-f = open(os.path.join(directory, 'example.ics'), 'wb')
-f.write(cal.to_ical())
-f.close()
+c = Calendar()
+e = Event()
+e.summary = "My cool event"
+e.description = "A meaningful description"
+e.begin = datetime.fromisoformat("2022-06-06T12:05:23+02:00")
+e.end = datetime.fromisoformat("2022-06-06T13:05:23+02:00")
+c.events.add(e)
+c
+# Calendar(extra=Container('VCALENDAR', []), extra_params={}, version='2.0', prodid='ics.py 0.8.0-dev0 - http://git.io/lLljaA', scale=None, method=None, events=[Event(extra=Container('VEVENT', []), extra_params={}, timespan=EventTimespan(begin_time=datetime.datetime(2022, 6, 6, 12, 5, 23, tzinfo=datetime.timezone(datetime.timedelta(seconds=7200))), end_time=None, duration=None, precision='second'), summary=None, uid='ed7975c7-01f1-42eb-bfc4-435afd76b33d@ed79.org', description=None, location=None, url=None, status=None, created=None, last_modified=None, dtstamp=datetime.datetime(2022, 6, 6, 19, 28, 14, 575558, tzinfo=Timezone.from_tzid('UTC')), alarms=[], attach=[], classification=None, transparent=None, organizer=None, geo=None, attendees=[], categories=[])], todos=[])
+with open("my.ics", "w") as f:
+    f.write(c.serialize())
