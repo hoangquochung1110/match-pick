d2aa0f9f375515670b8b7d01c51cc68f67fa04e7
hoangquochung1110 <arsenalforeversince2007@gmail.com>
2024-09-06 00:49:37 +0700

    Continue arsenal_match_list_2024 spider

--- epl_scrapy/spiders/arsenal_match_list_spider.py
+++ epl_scrapy/spiders/arsenal_match_list_spider.py
@@ -1,39 +1,54 @@
+import re
+
 import scrapy
 from scrapy_playwright.page import PageMethod
+
+from epl_scrapy.items import MatchItem
 
 
 class ArsenalMatchListSpider(scrapy.Spider):
 
-    name = "arsenal match list"
+    name = "arsenal_match_list_2024"
     start_urls = ["https://www.premierleague.com/results?co=1&se=578&cl=1"]
+    custom_settings = {
+        # specifies exported fields and order in csv
+        "FEED_EXPORT_FIELDS": [
+            "match_id",
+            "gameweek",
+            "home",
+            "away",
+            "fulltime_score",
+            "halftime_score",
+            "kickoff",
+            "referee",
+        ],
+    }
 
     def start_requests(self):
         yield scrapy.Request(
             url=self.start_urls[0],
             callback=self.parse_latest_match,
             meta=dict(
-                        playwright=True,
-                        playwright_include_page=True,
-                        playwright_page_methods=[
-                                    PageMethod("evaluate", "document.querySelector('#onetrust-accept-btn-handler').click()"),
-                                    PageMethod("evaluate", "document.querySelector('#advertClose').click()"),
-                                    PageMethod("evaluate", "window.scrollBy(0, 500)"),
-                                     # This where we can implement scrolling if we want
-                                     PageMethod(
-                                         'wait_for_selector', 'span.match-fixture__container')
-                                ]
+                playwright=True,
+                playwright_include_page=True,
+                playwright_page_methods=[
+                    PageMethod("evaluate", "document.querySelector('#onetrust-accept-btn-handler').click()"),
+                    PageMethod("evaluate", "document.querySelector('#advertClose').click()"),
+                    PageMethod("evaluate", "window.scrollBy(0, 500)"),
+                    # This where we can implement scrolling if we want
+                    PageMethod('wait_for_selector', 'span.match-fixture__container'),
+                ]
             )        
         )
     
     def parse_latest_match(self, response):
         """Get match_id of latest finished match."""
         matches = response.xpath("//li[@class='match-fixture']")
-        latest_match = None
-        for match in matches:
-            if match.attrib.get("data-comp-match-item-status") == "U":
-                break
-            latest_match = match
-        if latest_match:
-            yield {"match_id": match.attrib.get("data-comp-match-item")}
-        # Return -1 if the season hasn't started yet.
-        yield {"match_id": -1}
+        for index, match in enumerate(matches):
+            match_item = MatchItem()
+            match_item["match_id"] = match.attrib.get("data-comp-match-item")
+            score_text = match.xpath("(//span[@class='match-fixture__score '])")[index].get()
+            score_text = re.sub(r'<.*?>', '', score_text)
+            match_item["fulltime_score"] = score_text
+            yield match_item
+
